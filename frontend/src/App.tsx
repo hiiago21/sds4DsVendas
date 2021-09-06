44551e26f1a67dab2868036a2039559ceb225ce8
Hiago Silva <hiago.silva@sascar.com.br>
2021-09-06 19:20:26 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from "components/navbar";
+import Footer from "components/footer";
+import DataTable from "components/datatable";
+import { Fragment } from 'react';
+
 function App() {
   return (
-    <div >
-      <h1 >Olá mundo</h1>
-    </div>
+    <Fragment>
+      <NavBar></NavBar>
+      <div className = "container">
+        <h1 >Olá mundo</h1>
+      </div>
+      <DataTable></DataTable>
+      <Footer></Footer>
+    </Fragment>    
   );
 }
 
