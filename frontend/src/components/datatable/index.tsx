e8e2b13dee81f6c31c2c6e522d43d5c1385f5c49
Hiago Silva Da Silva <hiago.silva@meta.com.br>
2021-09-18 14:17:48 -0300

    Pagination

--- frontend/src/components/datatable/index.tsx
+++ frontend/src/components/datatable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 function DataTable() {
+
+const [activePage, setActivePage] = useState(0);
 
 const [page, setPage] = useState<SalePage>(
     {
@@ -17,37 +20,45 @@ const [page, setPage] = useState<SalePage>(
 )
 
 useEffect(()=>{
-    axios.get(`${BASE_URL}/sales?page=0&size=20&sorte=date,desc`)
+    axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sorte=date,desc`)
          .then(  (response)=>{
             setPage(response.data);
          });
-}, []);
+}, [activePage]);
+
+
+const changePage = (index:number)=>{
+    setActivePage(index);
+}
 
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Vendedor</th>
-                        <th>Clientes visitados</th>
-                        <th>Negócios fechados</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { page.content?.map(x => (
-                        <tr key = {x.id}>
-                        <td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
-                        <td>{x.seller.name}</td>
-                        <td>{x.visited}</td>
-                        <td>{x.deals}</td>
-                        <td>{x.amount.toFixed(2)}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <>  
+            <Pagination page = {page} onPageChange = {changePage}></Pagination>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Vendedor</th>
+                            <th>Clientes visitados</th>
+                            <th>Negócios fechados</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { page.content?.map(x => (
+                            <tr key = {x.id}>
+                            <td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
+                            <td>{x.seller.name}</td>
+                            <td>{x.visited}</td>
+                            <td>{x.deals}</td>
+                            <td>{x.amount.toFixed(2)}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
