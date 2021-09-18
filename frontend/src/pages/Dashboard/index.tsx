f66ebabccc65e7c6dfe4053a298b2a77a0665dc6
Hiago Silva Da Silva <hiago.silva@meta.com.br>
2021-09-18 15:55:55 -0300

    Correção na identação

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,34 +1,31 @@
-import NavBar from "components/navbar";
-import Footer from "components/footer";
-import DataTable from "components/datatable";
-import BarChart from "components/barchart";
-import DonutChart from "components/donutchart";
+import NavBar from "components/Navbar";
+import Footer from "components/Footer";
+import DataTable from "components/Datatable";
+import BarChart from "components/Barchart";
+import DonutChart from "components/Donutchart";
 
 function Dashboard() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboard</h1>
-        <br />
-        <div className="text-center">
-          <h2 className="text-primary">All Live Movimentations</h2>
-        </div>
-        <DataTable />
-        <br />
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Movimentation Pieces</h5>
-            <DonutChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success Rate on Clients trainings (%)</h5>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1> {/**py: padding vertical */}
+
+        <div className="row px-3">{/*} px: Padding horizontal */}
+          <div className="col-sm-6" >{/*col-sm-6: espaço de grade de 6 unidades */}
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
+          <div className="col-sm-6" >{/*col-sm-6: espaço de grade de 6 unidades */}
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
         </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <DataTable />
       </div>
-      <br />
-      <br />
       <Footer />
     </>
 
